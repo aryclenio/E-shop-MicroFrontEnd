9e66ab070d6d39c5036686b99d75ee2ffe30254e
Ary <aryclenio.barros@truckpad.com.br>
2021-05-03 12:07:49 -0300

    ✨ Reformulating project

--- cart/src/bootstrap.js
+++
@@ -1,5 +0,0 @@
-import faker from "faker";
-
-const cartText = `<div>You have ${faker.random.number()} items in cart</div>`;
-
-document.querySelector("#dev-cart").innerHTML = cartText;
--- container/src/bootstrap.js
+++
@@ -1,2 +0,0 @@
-import "products/ProductsIndex";
-import "cart/CartShow";
