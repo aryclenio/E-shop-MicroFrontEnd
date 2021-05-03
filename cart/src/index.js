bf4de5b475fc1be8c1ce9c6f241d752c52a26ec4
Ary <aryclenio.barros@truckpad.com.br>
2021-05-03 11:21:17 -0300

    ⚡️ Improving async loading libs

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,5 +1,1 @@
-import faker from "faker";
-
-const cartText = `<div>You have ${faker.random.number()} items in cart</div>`;
-
-document.querySelector("#dev-cart").innerHTML = cartText;
+import("./bootstrap");
