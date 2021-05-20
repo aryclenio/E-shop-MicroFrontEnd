4fd4bbb06c6d56d914e190d83b4e9be67374deef
Ary <aryclenio.barros@truckpad.com.br>
2021-05-20 08:34:26 -0300

    🐛 Fix generated classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,21 @@
+import { createGenerateClassName, StylesProvider } from '@material-ui/styles';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
+  const GenerateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+  });
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={GenerateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
